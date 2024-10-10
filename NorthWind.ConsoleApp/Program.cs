89ed149ae6875ea1fc3fe22623ef358384d4106f
jhon ramirez <stevenramirez1115@gmail.com>
2024-10-10 14:30:57 -0500

    video S4 parte1 continuacion

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -2,7 +2,7 @@
 using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
 
-IUserActionWriter Writer = default;
+IUserActionWriter Writer = new default;
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Aplicacion started.");
--- NorthWind.Entities/GlobalUsing.cs
+++ NorthWind.Entities/GlobalUsing.cs
@@ -1,12 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NorthWind.Entities
-{
-    internal class GlobalUsing
-    {
-    }
-}
+﻿global using NorthWind.Entities.Interfaces;
+global using NorthWind.Entities.ValueObjects;
