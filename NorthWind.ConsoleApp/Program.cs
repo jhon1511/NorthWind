6b544a112e2bd7a36e736098d59c99b79181815e
jhon ramirez <stevenramirez1115@gmail.com>
2024-10-10 18:14:03 -0500

    video S4 parte2 inicio

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,14 +1,25 @@
 ﻿
+using Microsoft.Extensions.Hosting;
 using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
+using Microsoft.Extensions.DependencyInjection;
+
+HostApplicationBuilder Builder = Host.CreateApplicationBuilder();
+Builder.Services.AddSingleton<IUserActionWriter, ConsoleWriter>();
+Builder.Services.AddSingleton<IUserActionWriter, DebugWriter>();
+Builder.Services.AddSingleton<IUserActionWriter, FileWriter>();
+Builder.Services.AddSingleton<AppLogger>();
+Builder.Services.AddSingleton<ProductService>();
+using var AppHost = Builder.Build();
 
 
-IUserActionWriter Writer = new DebugWriter();
 
-AppLogger Logger = new AppLogger(Writer);
+//IUserActionWriter Writer = new ConsoleWriter();
+
+AppLogger Logger = AppHost.Services.GetRequiredService<AppLogger>();
 Logger.WriteLog("Aplicacion started.");
 
-ProductService service = new ProductService(Writer);
+ProductService service = AppHost.Services.GetRequiredService<ProductService>();
 service.Add("Demo", "Azucar refinada");
 
